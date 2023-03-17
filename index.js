589a411e0601f0c9829010bf70c04b9ffcd58398
Alejandro Osorio <avot3110@hotmail.com>
2023-03-17 11:46:51 -0400

    arrow function

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (a,b) => {
+    console.log(`divide ${a}`);
+    console.log(`divide ${b}`);
+    return a / b;
+}
+divide(2000,100);
+
+function square(x){
+    console.log(`multiply ${x}`);
+    return Math.abs(x ** 2);
+}
+square(2);
+
+const add = (c,d) => {
+    console.log(`add ${c} ${d}`);
+    return c + d;
+}
+add(3,4)
